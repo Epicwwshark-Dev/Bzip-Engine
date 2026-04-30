885d0d002a9f8d4f437da1eee3054e23f7a89a58
Epicwwshark-Dev <rebeccawilliamsfam@gmail.com>
2026-04-30 07:56:52 -0700

    Update Bzip.bat

--- Bzip.bat
+++ Bzip.bat
@@ -1,27 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: --- SYSTEM BOOT ---
+:: --- SYSTEM BOOT: ANSI COLORS ---
+set "Gld=[93m" & set "Blu=[94m" & set "Red=[91m" & set "Grn=[92m" & set "Wht=[97m" & set "Rst=[0m"
+
+:: --- FOLDER SETUP ---
 set "vault=%localappdata%\BzipEngine"
 if not exist "%vault%" md "%vault%"
 set "config=%vault%\Bzip_Settings.cfg"
-set "ver=24.0"
+set "todofile=%vault%\Bzip_ToDo.txt"
+set "ver=25.0"
 
-:: --- YOUR OFFICIAL GITHUB LINK ---
-set "github_raw=https://raw.githubusercontent.com/Epicwwshark-Dev/Bzip-Engine/refs/heads/main/Bzip.bat"
-
-:: --- AUTO-UPDATE ENGINE ---
-title Bzip ENGINE [Checking for Updates...]
+:: --- GITHUB AUTO-PILOT ---
+set "github_raw=https://githubusercontent.com"
+:: Silent Update Check
 powershell -Command "$web = Invoke-WebRequest -Uri '!github_raw!' -UseBasicParsing; if($web.Content -match 'set \"ver=([0-9.]+)\"'){ $newVer = [float]$matches[1]; if($newVer -gt [float]!ver!){ exit 1 } else { exit 0 } }" >nul 2>&1
-
 if %errorlevel% EQU 1 (
-    cls
-    echo -----------------------------------------------------------
-    echo  [!] NEW VERSION FOUND. DOWNLOADING UPDATE...
-    echo -----------------------------------------------------------
     powershell -Command "Invoke-WebRequest -Uri '!github_raw!' -OutFile 'Bzip_Update.bat'"
     echo @echo off > updater.bat
-    echo timeout /t 2 /nobreak ^>nul >> updater.bat
+    echo timeout /t 1 /nobreak ^>nul >> updater.bat
     echo del "%~nx0" >> updater.bat
     echo ren "Bzip_Update.bat" "%~nx0" >> updater.bat
     echo start "" "%~nx0" >> updater.bat
@@ -30,56 +27,120 @@ if %errorlevel% EQU 1 (
     exit
 )
 
-:: --- LOAD SETTINGS ---
-set "maincol=0B"
+:: --- DEFAULT SETTINGS ---
+set "accent=%Gld%"
 set "owner_display=EPICWWSHARK"
 set "user_key=202020"
-set "stat_msg=OPERATIONAL"
+set "printer_access=OFF"
+set "beep=ON"
+
+:: Load Settings
 if exist "%config%" (for /f "usebackq tokens=1,2 delims==" %%a in ("%config%") do set "%%a=%%b") >nul 2>&1
 
 :: Window Setup
 mode con: cols=120 lines=45
-color !maincol!
 title Bzip ENGINE [!ver!]
+chcp 65001 >nul
 
 :password
 cls
-echo ----------------------------------------------------------------------------
+echo !accent!----------------------------------------------------------------------------!Rst!
 echo                            [ SECURITY CHECK ]
-echo ----------------------------------------------------------------------------
+echo !accent!----------------------------------------------------------------------------!Rst!
 echo.
-set /p "login=  ENTER ACCESS KEY: "
-if "!login!"=="232323434343" (set "access_level=Owner" & set "stat_msg=OWNER MODE" & goto menu)
-if "!login!"=="!user_key!" (set "access_level=User" & goto menu)
-if "!login!"=="202020" (set "access_level=User" & goto menu)
-goto password
+set /p "login=  !accent!ENTER ACCESS KEY: !Rst!"
+if "%login%"=="232323434343" (set "access_level=Owner" & set "stat_msg=!Red![ OWNER MODE ]!Rst!" & goto menu)
+if "%login%"=="!user_key!" (set "access_level=User" & set "stat_msg=!Grn!OPERATIONAL!Rst!" & goto menu)
+if "%login%"=="202020" (set "access_level=User" & set "stat_msg=!Grn!OPERATIONAL!Rst!" & goto menu)
+echo !Red!Invalid Key.!Rst! & timeout /t 2 >nul & goto password
 
 :menu
 cls
-echo ------------------------------------------------------------------------------------------------------------------------
+echo !accent!------------------------------------------------------------------------------------------------------------------------!Rst!
 echo   [ BZIP GOLD ENGINE ]                                                                        STATUS: !stat_msg!
-echo   VERSION: !ver!                                                                               TYPE: JAVA ENGINE
-echo ------------------------------------------------------------------------------------------------------------------------
-echo   OFFICIAL OWNER: !owner_display!  (!access_level!)
-echo ------------------------------------------------------------------------------------------------------------------------
+echo !accent!------------------------------------------------------------------------------------------------------------------------!Rst!
+echo   OFFICIAL OWNER: !accent!!owner_display!!Rst!  (!access_level!)
+echo !accent!------------------------------------------------------------------------------------------------------------------------!Rst!
 echo.
-echo    1] BUILD FULL 1.8.9      5] SCREENSHOTS    F] ASSET FINDER
-echo    2] BUILD FULL 26.1.2     6] PACK FOLDER    P] VERSION CONVERTER
-echo    3] AUTO-DEPLOY           7] PERF STATS     R] RESOLUTION CHECKER
-echo    4] DEBUG: VIEW LOGS      8] TO-DO LIST     L] LANG FILE GEN
+echo    !Blu!1]!Rst! BUILD FULL 1.8.9        !Blu!5]!Rst! OPEN SCREENSHOTS       !accent![F]!Rst! ASSET FINDER
+echo    !Blu!2]!Rst! BUILD FULL 26.1.2       !Blu!6]!Rst! PACK FOLDER            !accent![P]!Rst! VERSION CONVERTER
+echo    !Blu!3]!Rst! AUTO-DEPLOY ZIP         !Blu!7]!Rst! PERFORMANCE            !accent![R]!Rst! RESOLUTION CHECKER
+echo    !Blu!4]!Rst! DEBUG: GAME LOGS        !Blu!8]!Rst! TO-DO LIST             !accent![L]!Rst! LANG FILE GEN
 echo.
-echo    S] ENGINE SETTINGS       C] CLEAN          X] EXIT
+echo    !accent![S]!Rst! ENGINE SETTINGS                 !accent![C]!Rst! CLEAN WORKSPACE               !Red![X]!Rst! EXIT APP
 echo.
-set /p choice=  ENTER SELECTION: 
+set /p choice=  !accent!ENTER SELECTION: !Rst!
 
 if /i "!choice!"=="s" goto settings
+if /i "!choice!"=="8" goto todo
+if /i "!choice!"=="7" goto performance
 if /i "!choice!"=="x" exit
 goto menu
 
 :settings
 cls
-echo [ ENGINE SETTINGS ]
-echo  A] Color  B] Login Key  [X] SAVE ^& BACK
+echo !accent!----------------------------------------------------------------------------!Rst!
+echo   [ ENGINE SETTINGS ] - Access: !access_level!
+echo !accent!----------------------------------------------------------------------------!Rst!
+echo.
+echo   !Wht![ USER PARTITION ]!Rst!
+echo   A] Change Accent Color      B] Toggle Startup Beep: !beep!
+echo   C] Change YOUR Login Key    D] Reset Defaults
+echo.
+echo   !Wht![ PRINTER PARTITION ]!Rst!
+echo   E] Printer Access: !printer_access!
+echo.
+if "!access_level!"=="Owner" (
+    echo   !Red![ OWNER PARTITION ]!Rst!
+    echo   G] Set Owner Display Name   H] Emergency Factory Reset
+)
+echo.
+echo   !accent![X]!Rst! SAVE ^& BACK HOME
+echo !accent!----------------------------------------------------------------------------!Rst!
 set /p sc= Selection: 
-if /i "!sc!"=="x" ((echo maincol=!maincol!& echo user_key=!user_key!& echo owner_display=!owner_display!) > "%config%" & goto menu)
+
+if /i "%sc%"=="x" (
+    (echo accent=!accent!& echo printer_access=!printer_access!& echo user_key=!user_key!& echo owner_display=!owner_display!& echo beep=!beep!) > "%config%"
+    goto menu
+)
+if /i "%sc%"=="A" (set /p cp= Color Name: & if /i "!cp!"=="Red" set "accent=%Red%" & goto settings)
+if /i "%sc%"=="B" (if "!beep!"=="ON" (set "beep=OFF") else (set "beep=ON") & goto settings)
+if /i "%sc%"=="E" (if "!printer_access!"=="ON" (set "printer_access=OFF") else (set "printer_access=ON") & goto settings)
+
+if "!access_level!"=="Owner" (
+    if /i "%sc%"=="G" (set /p owner_display= New Name: & goto settings)
+    if /i "%sc%"=="H" (del "%config%" & exit)
+)
 goto settings
+
+:todo
+cls
+echo !accent!----------------------------------------------------------------------------!Rst!
+echo   [ BZIP TO-DO LIST ]
+echo !accent!----------------------------------------------------------------------------!Rst!
+if exist "%todofile%" (type "%todofile%") else (echo [List Empty])
+echo.
+echo  1] Add  2] Delete Note  3] !Blu!Print Notes!Rst!  [X] Back
+set /p tc= Action: 
+if "%tc%"=="1" (set /p "n=Note: " & echo - !n! >> "%todofile%" & goto todo)
+if "%tc%"=="2" (if exist "%todofile%" del "%todofile%" & goto todo)
+if "%tc%"=="3" (
+    if "!printer_access!"=="OFF" (echo !Red!PRINTER DISABLED IN SETTINGS.!Rst! & pause & goto todo)
+    copy /y "%todofile%" "%userprofile%\Desktop\Bzip_Notes.txt" >nul
+    echo !Grn!Notes printed to Desktop.!Rst! & pause & goto todo
+)
+if /i "%tc%"=="x" goto menu
+goto todo
+
+:performance
+cls
+powershell -Command "(Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory | ForEach-Object { Write-Host \"RAM Free: \" ([math]::Round($_ / 1MB, 2)) \"GB\" }"
+echo.
+echo  !Blu!P]!Rst! Print Report   !accent![X]!Rst! Back
+set /p pc= Choice: 
+if /i "%pc%"=="p" (
+    if "!printer_access!"=="OFF" (echo !Red!PRINTER DISABLED.!Rst! & pause & goto performance)
+    echo BZIP STATUS REPORT > "%userprofile%\Desktop\Bzip_Perf.txt"
+    echo !Grn!Report sent to Desktop.!Rst! & pause
+)
+goto menu
