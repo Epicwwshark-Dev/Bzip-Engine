2bbacef7b5d69790864045293f231cf7309614fa
Epicwwshark-Dev <rebeccawilliamsfam@gmail.com>
2026-04-30 13:42:21 -0700

    Update version to 31.1 and fix timeout command

--- Bzip.bat
+++ Bzip.bat
@@ -10,9 +10,9 @@ if not exist "%vault%" md "%vault%"
 set "config=%vault%\Bzip_Settings.cfg"
 set "keyfile=%vault%\Bzip_Key.sys"
 set "chatlog=%vault%\Bzip_Chat.txt"
-set "ver=31.0"
+set "ver=31.1"
 
-:: --- CLOUD LINKS (FIXED BY DEV) ---
+:: --- CLOUD LINKS (FIXED URLS) ---
 set "github_raw=https://githubusercontent.com"
 set "chat_url=https://githubusercontent.com"
 
@@ -25,7 +25,7 @@ if %errorlevel% EQU 1 (
     echo -----------------------------------------------------------
     powershell -Command "Invoke-WebRequest -Uri '!github_raw!' -OutFile 'Bzip_Update.bat'"
     echo @echo off > updater.bat
-    echo timeout /t 1 ^>nul >> updater.bat
+    echo timeout /t 1 /nobreak ^>nul >> updater.bat
     echo del "%~nx0" >> updater.bat
     echo ren "Bzip_Update.bat" "%~nx0" >> updater.bat
     echo start "" "%~nx0" >> updater.bat
@@ -61,7 +61,6 @@ set "login="
 set /p "login=  ENTER ACCESS KEY: "
 if "%login%"=="232323434343" (set "access_level=Owner" & set "stat_msg=%Red%[ OWNER MODE ]%Rst%" & goto menu)
 if "!login!"=="!user_key!" (set "access_level=User" & set "stat_msg=%Grn%OPERATIONAL%Rst%" & goto menu)
-if "!login!"=="bzip" (set "access_level=User" & set "stat_msg=%Grn%OPERATIONAL%Rst%" & goto menu)
 goto password
 
 :menu
@@ -79,7 +78,6 @@ echo    %Blu%4]%Rst% DEBUG LOGS       %Blu%8]%Rst% TO-DO LIST     %accent%[L]%Rs
 echo.
 set /p choice=  %accent%ENTER SELECTION: %Rst%
 
-:: Routing
 if /i "!choice!"=="s" goto settings
 if /i "!choice!"=="k" goto chat
 if /i "!choice!"=="x" exit
